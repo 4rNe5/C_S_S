311b87bf57d8bb511ff795fbb3ff77b3d84b28ff
박준현 <23wdwad@gmail.com>
2023-06-13 10:05:09 +0900

    Initial commit

---
+++ C_S_S/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for C_S_S project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'C_S_S.settings')
+
+application = get_asgi_application()
