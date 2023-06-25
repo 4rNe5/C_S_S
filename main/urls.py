07b45a0747884dba5c60c1d04354467e21cfb6ee
박준현 <23wdwad@gmail.com>
2023-06-25 15:07:32 +0900

    Finally Fin.
    Yooooooo

--- main/urls.py
+++ main/urls.py
@@ -7,13 +7,27 @@ from main.views import index, blog, posting, new_post, remove_post
 from django.conf.urls.static import static
 from django.conf import settings
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     # 웹사이트의 첫화면은 index 페이지이다 + URL이름은 index이다
+#     path('', index, name='index'),
+#     # URL:80/blog에 접속하면 blog 페이지 + URL이름은 blog이다
+#     path('blog/', blog, name='blog'),
+#     # URL:80/blog/숫자로 접속하면 게시글-세부페이지(posting)
+#     path('blog/<int:pk>/', posting, name='posting'),
+#     path('blog/new_post/', new_post, name='new_post'),
+#     path('blog/<int:pk>/remove/', remove_post),
+# ]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # 웹사이트의 첫화면은 index 페이지이다 + URL이름은 index이다
-    path('', index, name='index'),
     # URL:80/blog에 접속하면 blog 페이지 + URL이름은 blog이다
+    path('', blog, name='blog'),
     path('blog/', blog, name='blog'),
     # URL:80/blog/숫자로 접속하면 게시글-세부페이지(posting)
+    path('<int:pk>/', posting, name='posting'),
+    path('new_post/', new_post, name='new_post'),
+    path('<int:pk>/remove/', remove_post),
     path('blog/<int:pk>/', posting, name='posting'),
     path('blog/new_post/', new_post, name='new_post'),
     path('blog/<int:pk>/remove/', remove_post),
