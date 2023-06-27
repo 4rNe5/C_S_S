a883285e009946d94cc9279ddf431b80b64c4087
박준현 <23wdwad@gmail.com>
2023-06-27 14:27:09 +0900

    GG
    Gg

--- main/models.py
+++ main/models.py
@@ -3,8 +3,7 @@ from django.db import models
 class Post(models.Model):
     postname = models.CharField(max_length=50)
     kindcode = models.CharField(max_length=50, null=True)
-    # 게시글 Post에 이미지 추가
-    mainphoto = models.ImageField(blank=True, null=True)
+    mainphoto = models.ImageField(blank=True, null=True) # 
     contents = models.TextField()
 
     # postname이 Post object 대신 나오기
